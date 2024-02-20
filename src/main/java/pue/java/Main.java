6eaee6e67d112f094fa9fc60ffa193c70e37a680
Hanks3 <jimenez.cuesta.f@gmail.com>
2024-02-20 11:26:24 +0100

    Ahora guay del paraguay

--- src/main/java/pue/java/Main.java
+++ src/main/java/pue/java/Main.java
@@ -1,17 +1,15 @@
 package pue.java;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Raices r1 = new Raices(1,6,9);
+        Raices r2 = new Raices(4,10,2);
+        Raices r3 = new Raices(4,2,9);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("r1");
+        r1.calcular();
+        System.out.println("r2");
+        r2.calcular();
+        System.out.println("r3");
+        r3.calcular();
     }
 }
--- src/main/java/pue/java/Raices.java
+++ src/main/java/pue/java/Raices.java
@@ -1,2 +1,59 @@
-package pue.java;public class Raices {
+package pue.java;
+
+public class Raices {
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public Raices(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    //getDiscriminante(): devuelve el valor del discriminante (double), el discriminante tiene la siguiente formula, (b^2)-4*a*c
+    //tieneRaiz(): devuelve un booleano indicando si tiene una única solución, para que esto ocurra, el discriminante debe ser igual que 0.
+    //tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, para que esto ocurra, el discriminante debe ser mayor que 0.
+    //obtenerRaiz(): imprime única raíz, que será cuando solo tenga una solución posible.
+    //obtenerRaices(): imprime las 2 posibles soluciones
+    //calcular(): mostrara por consola las posibles soluciones que tiene nuestra ecuación, en caso de no existir solución, mostrarlo también.
+    //Formula ecuación 2º grado: (-b±√((b^2)-(4*a*c)))/(2*a)
+
+
+    public double getDiscriminante(){
+
+        double b2 = Math.pow(b,2);
+        return b2-(4*a*c);
+    }
+    public boolean tieneRaiz(){
+        return getDiscriminante() == 0;
+    }
+
+    public boolean tieneRaices(){
+
+        return getDiscriminante() > 0;
+    }
+
+    public void obtenerRaiz(){
+        double raiz = -b/2*a;
+        System.out.println("Unica Raiz: " + raiz);
+    }
+    public void obtenerRaices(){
+        double raizDiscriminante = Math.sqrt(getDiscriminante());
+        double raizPositiva = (-b+raizDiscriminante)/2*a;
+        double raizNegativa = (-b-raizDiscriminante)/2*a;
+        System.out.println("Raiz 1: " + raizPositiva);
+        System.out.println("Raiz 2: " + raizNegativa);
+    }
+
+    public void calcular(){
+        if (tieneRaiz()){
+            obtenerRaiz();
+        } else if (tieneRaices()) {
+            obtenerRaices();
+        }else {
+            System.out.println("La ecuación no tiene solucion Real");
+        }
+    }
+
 }
